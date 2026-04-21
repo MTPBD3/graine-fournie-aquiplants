926e70457b6b75f366dc4794f9398926b08084a9
‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ <valent1louve@gmail.com>
2026-04-21 11:23:59 +0200

    fix: test credentials vides attend 400 pas 401

--- backend/tests/Functional/GfClientControllerTest.php
+++ backend/tests/Functional/GfClientControllerTest.php
@@ -43,12 +43,12 @@ class GfClientControllerTest extends WebTestCase
         $this->assertResponseStatusCodeSame(401);
     }
 
-    public function testLoginAvecCredentialsVideRetourne401(): void
+    public function testLoginAvecCredentialsVideRetourne400(): void
     {
         $client = static::createClient();
         $client->request('POST', '/api/login', [], [], [
             'CONTENT_TYPE' => 'application/json',
         ], json_encode(['email' => '', 'password' => '']));
-        $this->assertResponseStatusCodeSame(401);
+        $this->assertResponseStatusCodeSame(400);
     }
 }
